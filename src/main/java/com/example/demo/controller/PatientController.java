b37032c8aa74eea4ee71a8c578077e92f904caca
Abhishek Malhotra <abhinm1801@gmail.com>
2026-05-27 12:09:25 +0530

    Converted JDBC to Spring JPA

--- src/main/java/com/example/demo/controller/PatientController.java
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -1,13 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.patientDAO;
+import com.example.demo.model.patient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PatientController {
 
+    @Autowired
+    private patientDAO dao;
+
     @GetMapping("/")
     public String home() {
         return "patient";
+    }
+
+    @PostMapping("/savePatient")
+    public String savePatient(
+            @RequestParam String name,
+            @RequestParam int age,
+            @RequestParam String gender,
+            @RequestParam String phone,
+            @RequestParam String address
+    ) {
+
+        patient p = new patient(
+                name,
+                age,
+                gender,
+                phone,
+                address
+        );
+
+        dao.save(p);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/demo/model/patient.java
+++ src/main/java/com/example/demo/model/patient.java
@@ -1,12 +1,23 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="patients")
 public class patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
     private int age;
     private String gender;
     private String phone;
     private String address;
+
+    public patient() {
+    }
 
     public patient(String name, int age,
                    String gender,
@@ -18,6 +29,10 @@ public class patient {
         this.gender = gender;
         this.phone = phone;
         this.address = address;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
